a0b613fb68e30c5e8051ceae3a5e40f4c0609b69
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-06-26 20:34:55 +0200

    Edit video tag

--- src/components/FeatureVideo.jsx
+++ src/components/FeatureVideo.jsx
@@ -37,6 +37,7 @@ export function FeatureVideo() {
                   fetchpriority="auto"
                   autoplayBoolean={true}
                   controlsBoolean={true}
+                  loopBoolean={true}
                 />
               </div>
               {/* Desktop */}
