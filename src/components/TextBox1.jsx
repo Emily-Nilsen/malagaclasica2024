18bec5abf39615647c88a6652230bb3d0edcdf36
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-09-03 21:34:32 +0200

    being updated

--- src/components/TextBox1.jsx
+++ src/components/TextBox1.jsx
@@ -13,13 +13,11 @@ export function TextBox1() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
           {/* change back to text-gray-600 */}
-          <p className="text-base font-semibold leading-7 text-gray-900">
-            {oppdatering}.
-          </p>
+
           {/* <h1 className="mt-2 text-3xl font-extrabold text-gray-900 uppercase sm:text-4xl">
             Erfarne Advokater
           </h1> */}
-          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+          <div className="mt-0 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
             <div>
               <motion.p
                 whileInView={{ opacity: 1 }}
@@ -59,14 +57,12 @@ export function TextBox1() {
               </motion.p>
             </div>
           </div>
-          {/* <div className="flex mt-10">
-            <a
-              href="#"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Get started
-            </a>
-          </div> */}
+
+          <div className="mt-12 flex w-fit bg-gray-50 px-8 py-6">
+            <p className="text-base font-semibold leading-7 text-gray-900">
+              {oppdatering}.
+            </p>
+          </div>
         </div>
       </div>
     </div>
