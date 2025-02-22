438d569c20b0e764a733c85b914663eaacaaa081
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2025-02-22 19:59:56 +0300

    document

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -2,15 +2,12 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className="h-full bg-white antialiased" lang="en">
+    <Html className="h-full antialiased bg-white" lang="en">
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/zgi1gdg.css" />
-        <link
-          rel="icon"
-          href="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/M%C3%A1laga%20Cl%C3%A1sica/favicon_qyejeq.svg"
-        />
+        <link rel="icon" href="/media/favicon_qyejeq.svg" />
       </Head>
-      <body className="flex h-full flex-col">
+      <body className="flex flex-col h-full">
         <Main />
 
         <NextScript />
