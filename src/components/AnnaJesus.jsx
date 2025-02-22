d3258d56613a9b9e116978e661f8ea90bb7bc47e
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2025-02-22 20:40:21 +0300

    anna jesus

--- src/components/AnnaJesus.jsx
+++ src/components/AnnaJesus.jsx
@@ -24,10 +24,10 @@ export function AnnaJesus() {
     <div className="">
       <div className="relative bg-pink3_2024/50"></div>
       <div className="bg-pink3_2024/50">
-        <div className="mx-auto max-w-2xl px-4 pb-24 pt-0 sm:px-6 sm:pb-32 sm:pt-0 lg:max-w-7xl lg:px-8">
-          <div className="grid grid-cols-1 items-center gap-x-8 gap-y-16 lg:grid-cols-2">
+        <div className="max-w-2xl px-4 pt-0 pb-24 mx-auto sm:px-6 sm:pb-32 sm:pt-0 lg:max-w-7xl lg:px-8">
+          <div className="grid items-center grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2">
             <div>
-              <div className="border-b border-pinkText2024/30 pb-10">
+              <div className="pb-10 border-b border-pinkText2024/30">
                 <div className="text-gray-600 ">
                   <p>{para_5}</p>
                   <p className="mt-5">{para_6}</p>
@@ -38,7 +38,7 @@ export function AnnaJesus() {
               <dl className="mt-10 space-y-10">
                 {directors.map((feature) => (
                   <div key={feature.name}>
-                    <h4 className="text-lg font-bold italic text-pinkText2024">
+                    <h4 className="text-lg italic font-bold text-pinkText2024">
                       {feature.name}
                     </h4>
                     <dd className="mt-3 text-base font-medium text-blueGrey2024">
@@ -50,7 +50,7 @@ export function AnnaJesus() {
             </div>
 
             <div>
-              <div className="mt-4 grid grid-cols-2 gap-4 sm:mt-6 sm:gap-6 lg:mt-8 lg:gap-8">
+              <div className="grid grid-cols-2 gap-4 mt-4 sm:mt-6 sm:gap-6 lg:mt-8 lg:gap-8">
                 <motion.div
                   initial={{
                     opacity: 0,
@@ -63,16 +63,16 @@ export function AnnaJesus() {
                     delay: 0,
                     ease: 'easeIn',
                   }}
-                  className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100"
+                  className="overflow-hidden bg-gray-100 rounded-lg aspect-h-1 aspect-w-1"
                 >
-                  <div className="h-full w-full object-cover object-center">
+                  <div className="object-cover object-center w-full h-full">
                     <Image
-                      src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Jes%C3%BAs%20Reina/general/image-03-small_ccpydm.jpg"
+                      src="/media/image-03-small_ccpydm.jpg"
                       alt="Jesús Reina"
                       unoptimized
                       width={500}
                       height={500}
-                      className="h-full w-full object-cover object-center"
+                      className="object-cover object-center w-full h-full"
                     />
                   </div>
                 </motion.div>
@@ -88,16 +88,16 @@ export function AnnaJesus() {
                     delay: 0.3,
                     ease: 'easeIn',
                   }}
-                  className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100"
+                  className="overflow-hidden bg-gray-100 rounded-lg aspect-h-1 aspect-w-1"
                 >
-                  <div className="h-full w-full object-cover object-center">
+                  <div className="object-cover object-center w-full h-full">
                     <Image
-                      src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/M%C3%A1laga%20Cl%C3%A1sica/anna-nilsen_kolwch.webp"
+                      src="/media/anna-nilsen_kolwch.webp"
                       alt="Anna Nilsen"
                       unoptimized
                       width={500}
                       height={500}
-                      className="h-full w-full object-cover object-center"
+                      className="object-cover object-center w-full h-full"
                     />
                   </div>
                 </motion.div>
